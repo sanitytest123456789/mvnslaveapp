745bc69c5db0ee815f5c27b4cd4590a5c6dfa8d0
testuser <san.itytest123456789@gmail.com>
2021-10-05 02:03:34 +0100

    added testNG to this build

--- src/test/java/com/mymuveapp/app/muveapptest.java
+++ src/test/java/com/mymuveapp/app/muveapptest.java
@@ -1,22 +1,28 @@
 package com.mymuveapp.app;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import static org.junit.Assert.*;
 
-public class muveapptest extends TestCase {
+import org.testng.annotations.Test;
 
-    public muveapptest( String testName){ super(testName);}
+public class muveapptest {
 
-    public static Test suite() {return new TestSuite(com.mymuveapp.app.muveapptest.class);
-    }
-@org.junit.Test
+@Test
 public void testApp() {
-        //assertTrue(false);
-        muveapp test = new muveapp();
-        String output = test.toString();
-        assertNotSame("Hello World",output);
+    System.out.println("Test one");
     }
+
+@Test
+public void testStep() {
+    System.out.println("Test two");
+}
+
+    @Test
+    public void testStepTwo() {
+        System.out.println("Test three");
+    }
+//    @Test
+//    void testStep(){
+//        System.out.println("Another test goes here");
+//        assertNotSame("this message",countTestCases());
+//    }
 }
 
